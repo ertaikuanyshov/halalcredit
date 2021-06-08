e38abdca90f10ab88d8493f7ccb9d9f5a88246c0
Ertai <ertai.master@gmail.com>
2021-06-09 01:11:41 +0500

    Updated ui Added first contribution

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,3 +1,7 @@
 int getMonthPay(int money, int month) {
   return money ~/ month;
 }
+
+int getFirstContribution(int money, int month) {
+  return (money ~/ 100) * 10;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:halalcredit/functions.dart';
+import 'package:superellipse_shape/superellipse_shape.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -11,8 +13,9 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.ubuntuTextTheme(),
       ),
       home: HomePage(),
     );
@@ -20,14 +23,13 @@ class App extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   double _currentMoney = 45000;
   double _month = 1;
 
@@ -35,65 +37,97 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("HalalCredit"),
+        title: const Text("Халал Кредит | Калкульятор"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: const Text(
-              'Получит займ без процентов, просрочек',
-              style: const TextStyle(fontSize: 20.0),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: const Text(
+                  'Получит займ без процентов, просрочек',
+                  style: const TextStyle(fontSize: 20.0),
+                ),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          Slider(
+            const SizedBox(
+              height: 30.0,
+            ),
+            Slider(
               value: _currentMoney,
               min: 45000,
               max: 2000000,
               divisions: 500,
               label: _currentMoney.toInt().toString(),
-              onChanged: (value) => setState(() => _currentMoney = value)),
-          Text(
-            _currentMoney.toInt().toString() + " тг",
-            style: const TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
-          ),
-          Slider(
-              value: _month,
-              min: 1,
-              max: 12,
-              divisions: 12,
-              label: _month.toInt().toString(),
-              onChanged: (value) => setState(() => _month = value)),
-          Text(
-            _month.toInt().toString() + " месяц",
-            style: const TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 50.0,
-          ),
-          Text(
-            "В месяц платите: " +
-                getMonthPay(_currentMoney.toInt(), _month.toInt()).toString() +
-                " тг",
-            style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 60.0,
-          ),
-          FlatButton(
-            padding: const EdgeInsets.all(18.0),
-            color: Colors.redAccent,
-            textColor: Colors.white,
-            child: const Text("Получит халал займ"),
-            onPressed: () => launch(
-              "https://cabinet.halalcredit.kz/ru/order/create?ref_id=48058804",
+              onChanged: (value) => setState(() => _currentMoney = value),
             ),
-          )
-        ],
+            Text(
+              _currentMoney.toInt().toString() + " тг",
+              style:
+                  const TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+            ),
+            Slider(
+                value: _month,
+                min: 1,
+                max: 12,
+                divisions: 12,
+                label: _month.toInt().toString(),
+                onChanged: (value) => setState(() => _month = value)),
+            Text(
+              _month.toInt().toString() + " месяц",
+              style:
+                  const TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text(
+              "В месяц платите: " +
+                  getMonthPay(_currentMoney.toInt(), _month.toInt())
+                      .toString() +
+                  " тг",
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "Первоначальный взнос: " +
+                  getFirstContribution(_currentMoney.toInt(), _month.toInt())
+                      .toString() +
+                  " тг",
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(
+              height: 60.0,
+            ),
+            MaterialButton(
+              padding: const EdgeInsets.only(
+                top: 18,
+                bottom: 18,
+                right: 25,
+                left: 25,
+              ),
+              color: Colors.redAccent,
+              textColor: Colors.white,
+              child: const Text(
+                "Получит халал займ",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              shape: SuperellipseShape(
+                borderRadius: BorderRadius.circular(28.0),
+              ),
+              onPressed: () => launch(
+                "https://cabinet.halalcredit.kz/ru/order/create?ref_id=48058804",
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
