fb6fc36252fb513b4543130a891ab92e46298a58
Ertai <ertai.master@gmail.com>
2021-06-25 00:45:57 +0500

    Updated config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,2 +1,4 @@
 const SITE_URL =
     "https://cabinet.halalcredit.kz/ru/order/create?ref_id=48058804";
+
+const TITLE = "Халал Кредит | Калкульятор";
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:halalcredit/components/open_link_button.dart';
+import 'package:halalcredit/config.dart';
 import 'package:halalcredit/functions.dart';
 
 import 'components/header_info.dart';
@@ -36,7 +37,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Халал Кредит | Калкульятор"),
+        title: const Text(TITLE),
       ),
       body: SingleChildScrollView(
         child: Column(
